87a969653731d692dd00f169abe75d0116ea8018
Győző Horváth <gyozo.horvath@inf.elte.hu>
2017-11-27 15:36:26 +0100

    generating recipe list

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'recipe-list',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   filterText: string = "";
+  recipes: Recipe[] = [
+    { id: 1, title: 'recipe1', ingredients: 'ingr1', description: 'desc1', imgUrl: 'http://www.mnhsz.com/storage/upload/2016/gulyas1.jpg' },
+    { id: 2, title: 'recipe2', ingredients: 'ingr2', description: 'desc2', imgUrl: 'http://www.nosalty.hu/files/imagecache/recept/recept_kepek/dsc_0421.jpg' },
+    { id: 3, title: 'recipe3', ingredients: 'ingr3', description: 'desc3', imgUrl: 'http://2.bp.blogspot.com/_5O-jypyl64M/TFJhfFqFYFI/AAAAAAAAD3o/SnHbicHMvM8/s1600/csusza.jpg' },
+    { id: 4, title: 'recipe4', ingredients: 'ingr4', description: 'desc4', imgUrl: 'http://www.nosalty.hu/files/imagecache/recept/recept_kepek/bajai-halaszle.jpg' },
+  ];
 
   constructor() { }
 
---
+++ src/app/recipe.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+  id: number = 0;
+  title: string = '';
+  ingredients: string = '';
+  description: string = '';
+  imgUrl: string = '';
+}
